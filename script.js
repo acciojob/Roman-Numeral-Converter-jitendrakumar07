730c99f944e383ad1ab9160cd6cd0657206b7916
JITENDRA KUMAR <jitendrakumar77800@gmail.com>
2025-02-20 17:13:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 function convertToRoman(num) {
-	 if (num < 0 || num > 100000) return "Out of range";
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
-
-  //your code here
-	let roman = "";
-
-    for (let [symbol, value] of symbols) {
-        while (num >= value) {
-            roman += symbol;
-            num -= value;
-        }
+  if (num < 0 || num > 100000) return "Out of range";
+  
+  // Define symbols including subtractive combinations
+  const symbols = [
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1]
+  ];
+  
+  let roman = "";
+  
+  // Construct the roman numeral string
+  for (let [symbol, value] of symbols) {
+    while (num >= value) {
+      roman += symbol;
+      num -= value;
     }
-
-    return roman;
+  }
+  
+  return roman;
 }
-
-}
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(convertToRoman(36));
-
-
-
 
 // do not edit below this line
-module.exports = convertToRoman
+module.exports = convertToRoman;
